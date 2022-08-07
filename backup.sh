631864f16ebe612a94ed04c02e8f89d728509846
Fabricio Silva <hi@fabricio.dev>
2022-08-07 07:23:24 +0100

    Fix glob matching with compgen

--- backup.sh
+++ backup.sh
@@ -23,7 +23,7 @@ export_backup() {
   local APPDIR="$2"
   local DB="$3"
 
-  local SOURCE="$(realpath "$APPDATA/$APPDIR/$DB")"
+  local SOURCE="$APPDATA/$APPDIR/$DB"
   local DEST="$TMPDIR/$APPDIR/$DB"
   echo -e "${GRAY}[$FILENAME] Exporting $(basename "$SOURCE") to temp_dir"
 
@@ -43,7 +43,8 @@ compress_backup() {
 
   # tar command explained
   #    c = create a new archive
-  #    I = use xz compression algorithm with options (-T0 for enable multi-thread)
+  #    I = compression algorithm options
+  #        xz compresstion with multi-thread enabled (-T0)
   #    f = specify file name
   #    --transform = use sed replace EXPRESSION to transform files (from inside the tar)
   #        "s,<find>,<replace>," format
@@ -52,6 +53,7 @@ compress_backup() {
   #    --absolute-names = don't strip leading '/'s from file names
   #        it is needed to apply our --transform sed (or need to change it)
   #    --ignore-failed-read = do not exit with nonzero on unreadable files
+  #        if something goes wrong it will not stop the tar, but you should always verify that
   tar -cI "xz -T0" -f "$BACKUPS/$TARFILE" \
     --transform "s,^$TMPDIR/,,;s,^$APPDATA/,," \
     --absolute-names --ignore-failed-read \
@@ -78,9 +80,9 @@ vaultwarden() {
   local FILENAME="vaultwarden" # backup file name to create inside $BACKUPS
   local APPDIR="vaultwarden"   # directory inside $APPDATA
   local DBS=("db.sqlite3")     # database to export (inside $APPDATA)
-  local FILES=(                # files to compress (inside $APPDATA)
+  local FILES=(                # files and directories to send to tar (inside $APPDATA)
     "config.json"              # file
-    "rsa_key*"                 # glob pattern allowed
+    "rsa_key*"                 # glob pattern (for $FILES only) use wisely or avoid
     "attachments"              # directory
     "sends"                    # directory
   )
@@ -89,9 +91,11 @@ vaultwarden() {
   echo -e "${YELLOW}[$FILENAME] Started"
   local ARGS=() # will keep the files to pass to tar
   for DB in "${DBS[@]}"; do
-    export_backup "$FILENAME" "$APPDIR" "$DB"; ARGS+=("$(realpath "$TMPDIR/$APPDIR/$DB")");
+    export_backup "$FILENAME" "$APPDIR" "$DB"; ARGS+=("$TMPDIR/$APPDIR/$DB");
   done
-  for FILE in "${FILES[@]}"; do ARGS+=("$(realpath "$APPDATA/$APPDIR/$FILE")"); done
+  for FILE in "${FILES[@]}"; do
+    local GLOB=$(compgen -G "$APPDATA/$APPDIR/$FILE"); ARGS+=("${GLOB[@]}");
+  done
   compress_backup "$FILENAME" "${ARGS[@]}"
   remove_tmp "$FILENAME" "$APPDIR"
   echo -e "${GREEN}[$FILENAME] Finished"
@@ -116,16 +120,18 @@ plex() {
   )
   local FILES=(
     "Preferences.xml"
-    "Metadata"
+    "Metadata" # directory
   )
 
   # start the script
   echo -e "${YELLOW}[$FILENAME] Started"
   local ARGS=() # will keep the files to pass to tar
   for DB in "${DBS[@]}"; do
-    export_backup "$FILENAME" "$APPDIR" "$DB"; ARGS+=("$(realpath "$TMPDIR/$APPDIR/$DB")");
+    export_backup "$FILENAME" "$APPDIR" "$DB"; ARGS+=("$TMPDIR/$APPDIR/$DB");
   done
-  for FILE in "${FILES[@]}"; do ARGS+=("$(realpath "$APPDATA/$APPDIR/$FILE")"); done
+  for FILE in "${FILES[@]}"; do
+    local GLOB=$(compgen -G "$APPDATA/$APPDIR/$FILE"); ARGS+=("${GLOB[@]}");
+  done
   compress_backup "$FILENAME" "${ARGS[@]}"
   remove_tmp "$FILENAME" "$APPDIR"
   echo -e "${GREEN}[$FILENAME] Finished"
